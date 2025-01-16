0df2a544a86994524e8a4e484233f7d06467a0dd
Darkfenix2025 <dariojavierramirez@gmail.com>
2025-01-15 22:27:25 -0300

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
             chatLog.innerHTML += `<p class="user-message">${userMessage}</p>`;
             userInput.value = '';
          try {
-               const response = await fetch('https://chatbot-legalito02.vercel.app/', {
+               const response = await fetch('https://chatbot-legalito02.vercel.app/api/chat.js', {
                   method: 'POST',
                   headers: {
                      'Content-Type': 'application/json',
